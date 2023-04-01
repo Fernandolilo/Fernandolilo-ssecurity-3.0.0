20786c1da05ea11b668217bb52e6346c0af16f26
Fernandolilo <fernando.sistenpro@outlook.com>
2023-04-01 18:33:34 -0300

    Extrac all token

--- src/main/java/com/systempro/auth/security/JWTService.java
+++ src/main/java/com/systempro/auth/security/JWTService.java
@@ -4,6 +4,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -23,18 +24,25 @@ public class JWTService {
 		return generateToken(new HashMap<>(), userDetails);
 	}
 
-	 public String extractUsername(String token) {
-		    return extractClaim(token, Claims::getSubject);
-		  }
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-		return Jwts.builder()
-				.setClaims(null)
-				.setSubject(userDetails.getUsername())
+		return Jwts.builder().setClaims(null).setSubject(userDetails.getUsername())
 				.setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-				.signWith(getSignInKey(), SignatureAlgorithm.HS256)
-				.compact();
+				.signWith(getSignInKey(), SignatureAlgorithm.HS256).compact();
 	}
 
 	private Key getSignInKey() {
