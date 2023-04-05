110a7b3abcc0b89f5eccd455ca62b802898bca13
Fernandolilo <fernando.sistenpro@outlook.com>
2023-04-05 09:49:12 -0300

    test environment released libary BD test

--- src/main/java/com/systempro/auth/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/systempro/auth/security/configuration/SecurityConfiguration.java
@@ -1,18 +1,36 @@
 package com.systempro.auth.security.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+	
+	private final Environment env;
+	
+
+	public SecurityConfiguration(Environment env) {
+		this.env = env;
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		// test environment released;
+				if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+					http.authorizeHttpRequests().requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+							.and().csrf().ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).and()
+							.headers(headers -> headers.frameOptions().sameOrigin());
+				}
 		return http.build();
 
 	}
