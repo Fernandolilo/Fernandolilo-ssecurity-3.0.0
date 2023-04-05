6edc50e248e5fe5f7d5a39ee1786d0ad369713cd
Fernandolilo <fernando.sistenpro@outlook.com>
2023-04-05 10:18:02 -0300

    UserSecurityDetails para implementação de validação

--- src/main/java/com/systempro/auth/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/systempro/auth/security/configuration/SecurityConfiguration.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -31,7 +33,16 @@ public class SecurityConfiguration {
 							.and().csrf().ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).and()
 							.headers(headers -> headers.frameOptions().sameOrigin());
 				}
-		return http.build();
+				return http.csrf().disable()
+		                .authorizeHttpRequests()
+		                .requestMatchers("/users/register").permitAll()
+		                .anyRequest()
+		                .authenticated().and()
+		                .sessionManagement()
+		                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		                .and()
+		                .build();
+			
 
 	}
 
