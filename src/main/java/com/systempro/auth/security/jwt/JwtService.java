7dbd29de4c0db744539cccc591cd27563a538532
Fernandolilo <fernando.sistenpro@outlook.com>
2023-04-06 07:24:22 -0300

    Extract Username do token

--- src/main/java/com/systempro/auth/security/jwt/JwtService.java
+++ src/main/java/com/systempro/auth/security/jwt/JwtService.java
@@ -7,32 +7,38 @@ import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
 
 	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
-	//gera token
-	 public String generateToken(String userName){
-	        Map<String,Object> claims=new HashMap<>();
-	        return createToken(claims,userName);
+	
+	// estrair o username do token//
+	 public String extractUsername(String token) {
+	        return extractClaim(token, Claims::getSubject);
 	    }
+	
+	// gera token
+	public String generateToken(String userName) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userName);
+	}
 
-	private String createToken(Map<String, Object> claims, String userName) {	
-		return Jwts
-				.builder()
-				.setClaims(claims)
-				.setSubject(userName)
-				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 *60*24))
+	private String createToken(Map<String, Object> claims, String userName) {
+		return Jwts.builder().setClaims(claims).setSubject(userName).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
 				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
 	}
 
+	// decofigica o secretKey == token
 	private Key getSignKey() {
-		// TODO Auto-generated method stub
-		return null;
+		byte[] keybytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keybytes);
 	}
 }
