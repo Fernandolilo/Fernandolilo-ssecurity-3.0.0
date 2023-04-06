792c76582a88f9805a176e5d6ba7160e647b26fc
Fernandolilo <fernando.sistenpro@outlook.com>
2023-04-06 07:33:02 -0300

    Validate Token

--- src/main/java/com/systempro/auth/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/systempro/auth/security/jwt/JwtAuthenticationFilter.java
@@ -9,54 +9,48 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.systempro.auth.security.UserDetailsServiceImpl;
-import com.systempro.auth.security.UserSecurityDetails;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
-	
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
 	private final UserDetailsServiceImpl userDetailsServiceImpl;
 	private final JwtService jwtService;
-	
-	
 
 	public JwtAuthenticationFilter(UserDetailsServiceImpl userDetailsServiceImpl, JwtService jwtService) {
 		this.userDetailsServiceImpl = userDetailsServiceImpl;
 		this.jwtService = jwtService;
 	}
 
-
-
 	@Override
-	protected void doFilterInternal(
-			HttpServletRequest request, 
-			HttpServletResponse response, 
-			FilterChain filterChain)
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-	
-		//pegando o Header
-		String authHeader = request.getHeader("Authorization");
-        String token = null;
-        String username = null;
-        //verificar o header e se ha o bearer caso haja sprita 7 caracteres 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            token = authHeader.substring(7);
-            username = jwtService.extractUsername(token);
-        }
 
-        // verifica usuario e as autorizações e se são diferentes de nulo
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-        	//faz a busca no email do usuario  para gerar o token
-            UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
-            if (jwtService.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
-            }
-        }
-        filterChain.doFilter(request, response);
-    }
+		// pegando o Header
+		String authHeader = request.getHeader("Authorization");
+		String token = null;
+		String username = null;
+		// verificar o header e se ha o bearer caso haja sprita 7 caracteres
+		if (authHeader != null && authHeader.startsWith("Bearer ")) {
+			token = authHeader.substring(7);
+			username = jwtService.extractUsername(token);
+		}
+
+		// verifica usuario e as autorizações e se são diferentes de nulo
+		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			// faz a busca no email do usuario para gerar o token
+
+			UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
+			if (jwtService.validateToken(token, userDetails)) {
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+						null, userDetails.getAuthorities());
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
 }
